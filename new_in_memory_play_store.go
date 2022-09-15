deb2d23b7f8f1a787e426cc1051f53b116caa2e9
Matthew Jackson <matthew.jackson@lampoo.com>
2022-09-15 19:12:20 +0100

    feat: add router and getLeagues service with tests

--- new_in_memory_play_store.go
+++ new_in_memory_play_store.go
@@ -11,7 +11,7 @@ func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 
 type InMemoryPlayerStore struct {
 	store map[string]int
-	lock sync.RWMutex
+	lock  sync.RWMutex
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
@@ -24,4 +24,12 @@ func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	i.lock.RLock()
 	defer i.lock.RUnlock()
 	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
